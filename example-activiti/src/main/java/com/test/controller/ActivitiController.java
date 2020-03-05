08680e816f2ed75dc8dead8dcd4ac7af416c74aa
renjianwei <hql1644@dingtalk.com>
2020-03-05 10:09:20 +0800

    增加项目

--- example-activiti/src/main/java/com/test/controller/ActivitiController.java
+++ example-activiti/src/main/java/com/test/controller/ActivitiController.java
@@ -6,8 +6,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -17,15 +23,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * @Date 2020/1/17
  **/
 @Controller
-@RequestMapping("/activitiController")
+@RequestMapping("/index")
 public class ActivitiController {
     private final static Logger logger = LoggerFactory.getLogger(ActivitiController.class);
 
     @Autowired
     private ActivitiConsumerService activitiConsumerService;
 
-    @RequestMapping(value = "/startActivitiDemo",method = RequestMethod.GET)
-    public String startActivitiDemo() {
+    @RequestMapping(value = "/index",method = RequestMethod.GET)
+    public String index(Model model) {
+        return "index";
+    }
+
+
+
+    @ResponseBody
+    @RequestMapping(value = "/startActiviti",method = RequestMethod.GET)
+    public String startActiviti(Model model) {
         activitiConsumerService.startActivitiDemo();
         return "ok";
     }
--- example-activiti/src/main/java/com/test/service/impl/ActivitiConsumerServiceImpl.java
+++ example-activiti/src/main/java/com/test/service/impl/ActivitiConsumerServiceImpl.java
@@ -5,17 +5,14 @@ import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
-import org.activiti.engine.impl.persistence.entity.ExecutionEntity;
-import org.activiti.engine.repository.DeploymentBuilder;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.DeploymentQuery;
+import org.activiti.engine.repository.ProcessDefinition;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -33,11 +30,23 @@ public class ActivitiConsumerServiceImpl implements ActivitiConsumerService {
     private TaskService taskService;
     @Autowired
     private ProcessEngine processEngine;
-
+    @Autowired
+    private RepositoryService repositoryService;
     @Override
     public void startActivitiDemo() {
         System.out.println("method startActivityDemo begin....");
-        // 开始流程
+
+//
+//        //通过流程文件名获得id，再通过id启动
+//        Deployment deploy = repositoryService.createDeployment().addClasspathResource("processes/shenhe.bpmn").deploy();
+//        // 获取流程定义
+//        ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery()
+//                .deploymentId(deploy.getId()).singleResult();
+//        // 启动流程定义，返回流程实例
+//        ProcessInstance pi = runtimeService.startProcessInstanceById(processDefinition.getId());
+
+
+        // 直接通过流程的id开始流程
         ProcessInstance pi = runtimeService.startProcessInstanceByKey("shenhe");
         String processId = pi.getId();
 
@@ -52,4 +61,6 @@ public class ActivitiConsumerServiceImpl implements ActivitiConsumerService {
         }
 
     }
+
+
 }
